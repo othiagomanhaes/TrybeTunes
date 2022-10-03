c780dbadeefeb08be7df7dd06fb9c8c190809c3f
othiagomanhaes <othiagomanhaes@gmail.com>
2022-10-03 16:20:14 -0300

    Correção do requisito 1

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Album extends React.Component {
   render() {
+    const { match: { params: { id } } } = this.props;
     return (
       <div data-testid="page-album">
-        Album
+        { `Album ${id}` }
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Album;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,60 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cantLog: true,
+      login: '',
+    };
+  }
+
+  habilitaBtnEnter = () => {
+    const { login } = this.state;
+    const minCarac = 3;
+    const possoLogar = login.length >= minCarac;
+    if (possoLogar) {
+      this.setState({
+        cantLog: false,
+      });
+    } else {
+      this.setState({
+        cantLog: true,
+      });
+    }
+  };
+
+  mudaBtn = ({ target }) => {
+    const { name, type, checked } = target;
+    const value = type === 'checkbox' ? checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => this.habilitaBtnEnter());
+  };
+
   render() {
+    const { cantLog, login } = this.state;
     return (
       <div data-testid="page-login">
-        Login
+        <form>
+          <input
+            data-testid="login-name-input"
+            type="text"
+            name="login"
+            value={ login }
+            onChange={ this.mudaBtn }
+          />
+
+          <button
+            id="btn-enter"
+            data-testid="login-submit-button"
+            type="button"
+            disabled={ cantLog }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
