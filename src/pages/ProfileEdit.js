6a4fb18058e4fad26777fe79c87b5684ffa32b53
othiagomanhaes <othiagomanhaes@gmail.com>
2022-10-10 14:30:42 -0300

    Requisito 14

--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Loading from './Loading';
-import { getUser } from '../services/userAPI';
+import { getUser, updateUser } from '../services/userAPI';
 
 class ProfileEdit extends React.Component {
   constructor() {
@@ -11,6 +11,7 @@ class ProfileEdit extends React.Component {
       user: undefined,
       email: undefined,
       image: undefined,
+      forbiddenButton: false,
     };
   }
 
@@ -27,26 +28,75 @@ class ProfileEdit extends React.Component {
       email: resp.email,
       image: resp.image,
       description: resp.description,
+      forbiddenButton: true,
     });
   };
 
-  habilitaBtn = () => {
+  attUser = async () => {
     const { user, email, image, description } = this.state;
-    const arr = [user, email, image, description];
-    const resp = arr.every((input) => input.length > 0);
-    return resp;
+    const { history } = this.props;
+    const obj = {
+      name: user,
+      email,
+      image,
+      description,
+    };
+    // this.setState({ isLoading: true });
+    await updateUser(obj);
+    // this.setState({ isLoading: false });
+    history.push('/profile');
+  };
+
+  validarEmail = (email) => { // Coment 1
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  habilitaBtnSave = () => {
+    const { user, email, image, description } = this.state;
+    const min = 3;
+    const imageOk = image.length > min;
+    const userOk = user.length > min;
+    const emailOk = this.validarEmail(email);
+    const descriptionOk = description.length > min;
+
+    if (userOk && emailOk && imageOk && descriptionOk) {
+      this.setState({ forbiddenButton: false });
+    } else {
+      this.setState({ forbiddenButton: true });
+    }
+  };
+
+  attEstado = ({ target }) => {
+    const { name, type, checked } = target;
+    const value = type === 'checkbox' ? checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => this.habilitaBtnSave());
   };
 
   render() {
-    const { user, email, image, description, isLoading } = this.state;
+    const { user, email, image, description, isLoading, forbiddenButton } = this.state;
     return (
       <div data-testid="page-profile-edit">
         <Header />
+        <p>Editar perfil</p>
         { isLoading
           ? <Loading />
           : (
             <form>
-              <img data-testid="edit-input-image" src={ image } alt={ user } />
+              <img
+                src={ image }
+                alt={ user }
+                onChange={ this.attEstado }
+              />
+              <input
+                type="text"
+                name="image"
+                value={ image }
+                onChange={ this.attEstado }
+                data-testid="edit-input-image"
+              />
 
               <p>Nome</p>
               <label htmlFor="editUser">
@@ -54,6 +104,7 @@ class ProfileEdit extends React.Component {
                   id="editUser"
                   name="user"
                   value={ user }
+                  onChange={ this.attEstado }
                   data-testid="edit-input-name"
                 />
               </label>
@@ -62,18 +113,21 @@ class ProfileEdit extends React.Component {
               <label htmlFor="editEmail">
                 <input
                   id="editEmail"
+                  type="email"
                   name="email"
                   value={ email }
+                  onChange={ this.attEstado }
                   data-testid="edit-input-email"
                 />
               </label>
 
               <p>Descrição</p>
               <label htmlFor="editDescription">
-                <input
+                <textarea
                   id="editDescription"
                   name="description"
                   value={ description }
+                  onChange={ this.attEstado }
                   data-testid="edit-input-description"
                 />
               </label>
@@ -81,7 +135,8 @@ class ProfileEdit extends React.Component {
               <button
                 data-testid="edit-button-save"
                 type="button"
-                disabled={ this.habilitaBtn }
+                disabled={ forbiddenButton }
+                onClick={ this.attUser }
               >
                 Salvar
               </button>
@@ -93,3 +148,5 @@ class ProfileEdit extends React.Component {
 }
 
 export default ProfileEdit;
+
+// Coments 1: https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
